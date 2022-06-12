6808eff834cb04589a7731436eda7557528a493f
KILLER.CODES <Killercodes@users.noreply.github.com>
2022-06-12 06:14:02 +0530

    Update serviceWorker.js

--- serviceWorker.js
+++ serviceWorker.js
@@ -1,7 +1,7 @@
 self.addEventListener("install", e => {
     e.waitUntil(
       caches.open("static").then(cache => {
-        return cache.addAll(['./','./logo.png','./style.css']);
+        return cache.addAll(['./','']);
       })
     );
   });
